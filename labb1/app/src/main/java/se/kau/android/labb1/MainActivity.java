e3169f9b9d5d14cb699e8aa7bc2ac94dde1e7630
Nour Ibrahim <saeed.ibrahim1989@gmail.com>
2021-09-02 23:27:35 +0200

    add som change in mainActivity

--- labb1/app/src/main/java/se/kau/android/labb1/MainActivity.java
+++ labb1/app/src/main/java/se/kau/android/labb1/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // this is fun
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
