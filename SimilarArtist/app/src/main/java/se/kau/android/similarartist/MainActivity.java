4c77d33dee96292992b2cf04d7350961c7ffdac3
Nour Ibrahim <saeed.ibrahim1989@gmail.com>
2021-10-07 19:04:43 +0200

    Update MainActivity.java

--- SimilarArtist/app/src/main/java/se/kau/android/similarartist/MainActivity.java
+++ SimilarArtist/app/src/main/java/se/kau/android/similarartist/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     @Override
