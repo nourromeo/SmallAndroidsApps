d385857a74ca41d70fc0b238e5f7f3a9d6b3aec7
Nour Ibrahim <saeed.ibrahim1989@gmail.com>
2021-10-07 19:04:08 +0200

    onCreate

--- SimilarArtist/app/src/main/java/se/kau/android/similarartist/MainActivity.java
+++ SimilarArtist/app/src/main/java/se/kau/android/similarartist/MainActivity.java
@@ -1,8 +1,10 @@
 package se.kau.android.similarartist;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.os.StrictMode;
 import android.util.Log;
 import android.view.View;
@@ -16,3 +18,11 @@ import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.net.URL;
 import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
