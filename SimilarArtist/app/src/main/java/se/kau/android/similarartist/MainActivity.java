8aeec382df17009b245472eb3f174a18ac234803
Nour Ibrahim <saeed.ibrahim1989@gmail.com>
2021-10-07 19:04:30 +0200

    Update MainActivity.java

--- SimilarArtist/app/src/main/java/se/kau/android/similarartist/MainActivity.java
+++ SimilarArtist/app/src/main/java/se/kau/android/similarartist/MainActivity.java
@@ -4,20 +4,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.os.StrictMode;
-import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
-
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserFactory;
-
-import java.net.URL;
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     @Override
