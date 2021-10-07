638fa152206d9e3f7cca8ef6177f85e42b4bebb1
Nour Ibrahim <saeed.ibrahim1989@gmail.com>
2021-10-07 19:00:35 +0200

    starting the app

---
+++ SimilarArtist/app/src/main/java/se/kau/android/similarartist/MainActivity.java
@@ -0,0 +1,18 @@
+package se.kau.android.similarartist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.net.URL;
+import java.util.ArrayList;
