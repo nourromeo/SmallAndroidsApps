c7a85e757f7e8b7bb1de61f01beeb099746d1179
Nour Ibrahim <saeed.ibrahim1989@gmail.com>
2021-10-18 17:49:21 +0200

    activity_similar build

--- SimilarArtist/app/src/main/java/se/kau/android/similarartist/MainActivity.java
+++ SimilarArtist/app/src/main/java/se/kau/android/similarartist/MainActivity.java
@@ -1,14 +1,36 @@
 package se.kau.android.similarartist;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    static final String TAG = "SimilarArtist";
+    private Intent intent;
+
+    public void onResume() {
+        super.onResume();
+
+        Log.d(TAG, "MainActivity onResume running");
+        intent=getIntent();
+    }
+
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "MainActivity onCreate running");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void StartClick(View v){
+        Log.d(TAG,"SpelaClick button onclick");
+
+        intent = new Intent(this, SimilarArtistActivity.class);
+        startActivity(intent);
+
+    }
+
 }
---
+++ SimilarArtist/app/src/main/java/se/kau/android/similarartist/SimilarArtistActivity.java
@@ -0,0 +1,14 @@
+package se.kau.android.similarartist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SimilarArtistActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_similar_artist);
+    }
+}
